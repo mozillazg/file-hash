2f1dfee19c256f072a2a87086e69fb2a94ed3e82
mozillazg <opensource.mozillazg@gmail.com>
2011-05-20 11:58:49 +0800

    更新注释

--- hash_value.py
+++ hash_value.py
@@ -6,26 +6,35 @@ import zlib
 import os
 from time import localtime, strftime
 
-# hash
+"""输入文件路径返回如下信息：
+File path:
+Size:
+Date modified:
+MD5:
+SHA1:
+CRC32:
+"""
+
+# hash 计算 MD5 、SHA1
 def hash_value(filename, filesize, maxsize, xhash):
     """提供要计算 hash 值的文件路径、文件大小、每次读取的文件块大小及
     算法的摘要对象
     返回字符串类型的 hash 值
     """
-    with open(filename, 'rb') as openfile:
-        if filesize < maxsize:
+    with open(filename, 'rb') as openfile: # 打开文件，一定要是以二进制打开
+        if filesize < maxsize: # 如果是小文件
             data = openfile.read()
             xhash.update(data)
-        else:
-            while True:
-                data = openfile.read(maxsize)
-                if not data:
+        else: # 大文件
+            while True: 
+                data = openfile.read(maxsize) # 读取文件块
+                if not data: # 直到读完文件
                     break
                 xhash.update(data)
     return xhash.hexdigest()
 
 
-# CRC32
+# CRC32 计算 CRC32
 def crc32_value(filename, filesize, maxsize):
     """提供要计算 CRC32 值的文件路径、文件大小及每次读取的文件块大小
     返回整型类型的 CRC32 值
@@ -45,8 +54,9 @@ def crc32_value(filename, filesize, maxsize):
 
 
 if __name__ == '__main__':
+    # TO DO: 去除 filepath 首尾的单引号及双引号
     filepath = raw_input('请输入文件路径：').strip().decode('utf-8')
-    blocksize = 1024 * 50
+    blocksize = 1024 * 1024 # 每次读取的文件块的大小（bytes）
     size = os.path.getsize(filepath) # 文件大小
     date = strftime('%Y/%m/%d %H:%M:%S', # 文件最后修改时间
                                 localtime(os.path.getmtime(filepath)))
